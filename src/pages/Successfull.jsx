d65a7c942538da3e8b9e3c06f541af38ac8a4dd9
DEEPA_1505 <deepikar4229@gmail.com>
2025-07-20 20:12:45 +0530

    first commit

---
+++ src/pages/Successfull.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Successfull = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-red-700 text-white text-3xl">
+      🎉 Login Successful! Welcome to Netflix Dashboard.
+    </div>
+  )
+}
+
+export default Successfull
